296de76ed1b7370c7240de01e9da2f544448ffea
Programming Bees <hasanakash799@gmail.com>
2022-10-12 23:44:21 +0600

    controling materials and moving

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 import './App.css';
-import Three from './components/three'
+import Three from './components/three';
 
 function App() {
   return (
     <div className="App">
-      Wellcome to Three JS World
+
       <Canvas id='three-canvas-container'>
         <Suspense fallback={null}>
           <Three/>
@@ -17,3 +17,5 @@ function App() {
 }
 
 export default App;
+
+// num-3 and len-12min
--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
+import React, { useEffect, useRef } from 'react';
+import { angleToRadians } from '../../utils/angle';
 
 const index = () => {
+
+  const orbitControlsRef = useRef(null);
+
+  useFrame((state) => {
+    // console.log("Comes from Mouse",state.mouse);
+    if (!!orbitControlsRef.current) {
+      const {x,y} = state.mouse;
+      console.log(-x * angleToRadians(90));
+      orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(45));
+      orbitControlsRef.current.setPolarAngle((y+1) * angleToRadians(90 - 30));
+      orbitControlsRef.current.update();
+    }
+  })
+
+  useEffect(() => {
+    if (!!orbitControlsRef.current) {
+      console.log(orbitControlsRef.current);
+    }
+  },[orbitControlsRef.current]);
   return (
     <>
-
-    <mesh>
-        <sphereGeometry args={[1,32,32]}/>
-        <meshStandardMaterial color="#ffffff"/>
+    <PerspectiveCamera makeDefault position={[0,1,5]} />
+    <OrbitControls ref={orbitControlsRef} minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(80)}/>
+    <mesh position={[0,1,0]}>
+        <sphereGeometry args={[0.75,32,32]}/>
+        <meshStandardMaterial color="#f4f"/>
     </mesh>
 
-    <mesh>
-        <planeGeometry args={[9,9]}/>
+    <mesh rotation={[-(angleToRadians(90)),0,0]}>
+        <planeGeometry args={[7,7]}/>
         <meshStandardMaterial color="#1ea3d8"/>
     </mesh>
     // Ambient Light
