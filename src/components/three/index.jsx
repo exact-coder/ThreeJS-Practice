513a5a97c2c9765dd9f3549838b4ba604552303b
Programming Bees <hasanakash799@gmail.com>
2022-10-14 04:09:25 +0600

    chore: giving annimation to my ball

--- src/components/three/index.jsx
+++ src/components/three/index.jsx
@@ -1,5 +1,6 @@
 import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
+import gsap from 'gsap';
 import React, { useEffect, useRef } from 'react';
 import * as THREE from "three";
 import { angleToRadians } from '../../utils/angle';
@@ -7,6 +8,8 @@ import { angleToRadians } from '../../utils/angle';
 const index = () => {
 
   const orbitControlsRef = useRef(null);
+  const ballRef = useRef(null);
+  
 
   useFrame((state) => {
     // console.log("Comes from Mouse",state.mouse);
@@ -24,11 +27,32 @@ const index = () => {
       console.log(orbitControlsRef.current);
     }
   },[orbitControlsRef.current]);
+
+  useEffect(() => {
+    if(!!ballRef.current){
+      const timeline = gsap.timeline({paused:true});
+
+      timeline.to(ballRef.current.position, {
+        x:1,
+        duration:2,
+        ease: "power3.out"
+      });
+
+      timeline.to(ballRef.current.position, {
+        y: 0.5,
+        duration: 2,
+        ease: "bounce.out"
+      }, "<");
+      timeline.play();
+    }
+  }, [ballRef.current])
+  
+
   return (
     <>
     <PerspectiveCamera makeDefault position={[0,1,5]} />
     <OrbitControls ref={orbitControlsRef} minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(80)}/>
-    <mesh position={[0,0.5,0]} castShadow>
+    <mesh position={[-2,2.5,0]} castShadow ref={ballRef}>
         <sphereGeometry args={[0.5,32,32]}/>
         <meshStandardMaterial color="#ffffff" metalness={0.7} roughness={0.2}/>
     </mesh>
